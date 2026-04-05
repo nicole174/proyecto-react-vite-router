b7f1b617cb2d1354ab909aff08dcc9eaad1ae055
Ximena Nicole Marmolejo Reyes <marmolejoximena0@gmail.com>
2026-04-05 17:53:15 -0600

    Inicializacion del proyecto

---
+++ src/components/Boton.jsx
@@ -0,0 +1,14 @@
+function Boton() {
+    return (
+        <div>
+            <h3>
+                Este es el boton mágico
+            </h3>
+            <button>
+                Click
+            </button>
+        </div>
+    )
+}
+
+export default Boton
