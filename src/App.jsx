babebd7585e38baaa02a5d2eb35d344f620ff231
Ximena Nicole Marmolejo Reyes <marmolejoximena0@gmail.com>
2026-04-05 18:27:41 -0600

    Creacion de paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,23 @@
-import { useState } from 'react'
-/* import reactLogo from './assets/react.svg'
+/*import { useState } from 'react'
+ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
  */
 import './App.css'
 import Boton from './components/Boton'
+import { BrowserRouter, Route, Router, Routes } from 'react-router'
+import Dashboard from './pages/Dashboard'
+import Contact from './pages/Contact'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Boton/>
-      <Boton/>
-      <Boton/>
-      <Boton/>
-      <Boton/>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Dashboard/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,8 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  
+
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <App />
   </StrictMode>,
 )
